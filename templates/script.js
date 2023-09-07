05e11d32de23ffe714f3ae899c8718ceada47727
Kryptonomic1 <greg@kryptonomic.io>
2023-09-07 17:28:19 -0400

    added GPT 4 CSS, JS and HTML Elements

--- templates/script.js
+++ templates/script.js
@@ -16,4 +16,37 @@ document.addEventListener("DOMContentLoaded", function() {
       body.classList.remove("dark-mode");
     }
   });
+
+  // Additional JavaScript for smooth scrolling or animations can go here
+  document.addEventListener("DOMContentLoaded", function() {
+    // Smooth scrolling
+    document.querySelectorAll('.menu a').forEach(anchor => {
+      anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: 'smooth'
+        });
+      });
+    });
+  
+    // Additional JavaScript for 3D animations can go here
+    document.addEventListener("DOMContentLoaded", function() {
+      // Get the main banner title element
+      const mainBannerTitle = document.querySelector('.main-banner h1');
+    
+      // Add 3D animation on mouseover
+      mainBannerTitle.addEventListener('mouseover', function() {
+        this.classList.add('animate-3d');
+      });
+    
+      // Remove 3D animation on mouseout
+      mainBannerTitle.addEventListener('mouseout', function() {
+        this.classList.remove('animate-3d');
+      });
+    });
+    
+  });
 });
